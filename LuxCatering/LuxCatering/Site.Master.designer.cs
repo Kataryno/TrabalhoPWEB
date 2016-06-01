b7069bf6d498e381f08387984061878f936ddd4b
Tiago <Tiago@DESKTOP-898RM0A>
2016-06-01 18:50:01 +0100

    connection

--- LuxCatering/LuxCatering/Site.Master.designer.cs
+++ LuxCatering/LuxCatering/Site.Master.designer.cs
@@ -22,6 +22,15 @@ namespace LuxCatering {
         protected global::System.Web.UI.WebControls.ContentPlaceHolder Stylesheets;
         
         /// <summary>
+        /// LoginView1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LoginView LoginView1;
+        
+        /// <summary>
         /// MainContent control.
         /// </summary>
         /// <remarks>
