4dcf00de35a245471e249d7d6854a832e41cfa30
Tiago <Tiago@DESKTOP-898RM0A>
2016-06-05 15:02:34 +0100

    fornecedores + clientes

---
+++ LuxCatering/LuxCatering/Fornecedores/FornecedoresForm.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LuxCatering
+{
+    public partial class ClientesForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void FormView1_ItemDeleted(object sender, FormViewDeletedEventArgs e)
+        {
+
+        }
+
+        protected void FormView1_ItemDeleting(object sender, FormViewDeleteEventArgs e)
+        {
+
+        }
+
+        protected void FormView1_ItemInserted(object sender, FormViewInsertedEventArgs e)
+        {
+
+        }
+
+        protected void FormView1_ItemInserting(object sender, FormViewInsertEventArgs e)
+        {
+
+        }
+
+        protected void GridView1_RowDeleted(object sender, GridViewDeletedEventArgs e)
+        {
+
+        }
+    }
+}
