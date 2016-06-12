c9bdee561e41b0f08971d9cf0620cf7508dd8ac8
ciberboy2000 <mmsimoes@mail.telepac.pt>
2016-06-12 23:48:39 +0100

    Implementação da atribuição da Role ao novo utilizador

--- LuxCatering/LuxCatering/Account/Register.aspx.cs
+++ LuxCatering/LuxCatering/Account/Register.aspx.cs
@@ -20,18 +20,30 @@ namespace LuxCatering.Account
             var user = new ApplicationUser() { UserName = Email.Text, Email = Email.Text };
             IdentityResult result = manager.Create(user, Password.Text);
 
-
             if (result.Succeeded)
             {
+                // Access the application context and create result variables.
+                Models.ApplicationDbContext context = new ApplicationDbContext();
+
+                if (rblPerfil.SelectedIndex == 0)
+                    RoleName = "Cliente";
+                else
+                    RoleName = "Fornecedor";
+
+                if (!manager.IsInRole(manager.FindByEmail(Email.Text).Id, RoleName))
+                {
+                    result = manager.AddToRole(manager.FindByEmail(Email.Text).Id, RoleName);
+                }
+
                 // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
                 //string code = manager.GenerateEmailConfirmationToken(user.Id);
                 //string callbackUrl = IdentityHelper.GetUserConfirmationRedirectUrl(code, user.Id, Request);
                 //manager.SendEmail(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>.");
 
-                signInManager.SignIn( user, isPersistent: false, rememberBrowser: false);
+                signInManager.SignIn(user, isPersistent: false, rememberBrowser: false);
                 IdentityHelper.RedirectToReturnUrl(Request.QueryString["ReturnUrl"], Response);
             }
-            else 
+            else
             {
                 ErrorMessage.Text = result.Errors.FirstOrDefault();
             }
